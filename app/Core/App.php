bfc3377ddb3fb72a44d54ad988e1c04645e33181
alchin07 <almobis@gmail.com>
2026-08-29 15:31:44 +0200

    Load delivery option input components

--- app/Core/App.php
+++ app/Core/App.php
@@ -25,8 +25,11 @@ class App
         require_once __DIR__ . '/../Controllers/OrderController.php';
         require_once __DIR__ . '/../Models/Order.php';
         require_once __DIR__ . '/../Models/Delivery.php';
+        require_once __DIR__ . '/../Models/DeliveryOptionInput.php';
         require_once __DIR__
-    . '/../Controllers/AdminDeliveryController.php'; 
+    . '/../Controllers/AdminDeliveryController.php';
+        require_once __DIR__
+    . '/../Controllers/AdminDeliveryOptionInputController.php';
 
         $router = new Router();
 
