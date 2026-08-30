e93222d8b85361503b51b54b3f92252daf0d6270
alchin07 <almobis@gmail.com>
2026-08-30 16:09:41 +0200

    Load product translator model

--- app/Core/App.php
+++ app/Core/App.php
@@ -16,6 +16,7 @@ class App
         require_once __DIR__ . '/../Models/Category.php';
         require_once __DIR__ . '/../Models/CategoryTranslator.php';
         require_once __DIR__ . '/../Models/Product.php';
+        require_once __DIR__ . '/../Models/ProductTranslator.php';
         require_once __DIR__ . '/../Models/Cart.php';
         require_once __DIR__ . '/../Models/User.php';
         require_once __DIR__ . '/../Models/Order.php';
