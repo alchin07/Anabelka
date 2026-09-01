37f803ac44bb63781bc0e3f05e70e58cb587a022
alchin07 <almobis@gmail.com>
2026-09-02 00:59:00 +0200

    Refresh admin navigation after highlight fix

--- views/partials/header.php
+++ views/partials/header.php
@@ -306,7 +306,7 @@ echo $cartCount;
 <?php if ($isAdminPage): ?>
 
     <script
-        src="/Anabelka/js/admin-nav.js?v=8"
+        src="/Anabelka/js/admin-nav.js?v=9"
     ></script>
 
 <?php endif; ?>
