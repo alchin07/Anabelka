ea9ff01bf2192ea68a1ced205b56db7b6776e28e
alchin07 <almobis@gmail.com>
2026-09-07 20:12:51 +0200

    Bump rank admin CSS cache version

--- views/admin/ranks/index.php
+++ views/admin/ranks/index.php
@@ -12,7 +12,7 @@ $escape = function ($value) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= $escape($pageTitle ?? 'Адмін-панель · Ранги') ?></title>
-    <link rel="stylesheet" href="/Anabelka/css/admin-ranks.css?v=3">
+    <link rel="stylesheet" href="/Anabelka/css/admin-ranks.css?v=4">
 </head>
 <body>
 
