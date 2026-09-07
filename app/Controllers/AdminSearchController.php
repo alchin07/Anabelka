c498b691dac7953d167d9c0ea54f221a28820e51
alchin07 <almobis@gmail.com>
2026-09-07 15:44:16 +0200

    Expose search analytics in admin

--- app/Controllers/AdminSearchController.php
+++ app/Controllers/AdminSearchController.php
@@ -10,6 +10,9 @@ class AdminSearchController extends Controller
         $rows = SearchQueryLog::page($filters, $page, $perPage);
         $total = SearchQueryLog::count($filters);
         $summary = SearchQueryLog::summary();
+        $periodAnalytics = SearchQueryLog::periodAnalytics();
+        $popularQueries = SearchQueryLog::popularQueries(30, 10);
+        $popularZeroQueries = SearchQueryLog::popularZeroQueries(30, 10);
         $pages = max(1, (int) ceil($total / $perPage));
 
         if ($page > $pages) {
@@ -22,6 +25,9 @@ class AdminSearchController extends Controller
             'rows' => $rows,
             'filters' => $filters,
             'summary' => $summary,
+            'periodAnalytics' => $periodAnalytics,
+            'popularQueries' => $popularQueries,
+            'popularZeroQueries' => $popularZeroQueries,
             'page' => $page,
             'pages' => $pages,
             'total' => $total
