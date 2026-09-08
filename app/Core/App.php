00b7941d1a6c11bee1e29a887f931e64a7323e3b
alchin07 <almobis@gmail.com>
2026-09-08 23:15:28 +0200

    Wire customer account module

--- app/Core/App.php
+++ app/Core/App.php
@@ -29,6 +29,8 @@ class App
         require_once __DIR__ . '/../Models/CartColorMigration.php';
         require_once __DIR__ . '/../Models/Favorite.php';
         require_once __DIR__ . '/../Models/User.php';
+        require_once __DIR__ . '/../Models/CustomerAccount.php';
+        require_once __DIR__ . '/../Models/CustomerAccountInterfaceTranslator.php';
         require_once __DIR__ . '/../Models/AdminUser.php';
         require_once __DIR__ . '/../Models/Order.php';
         require_once __DIR__ . '/../Models/QuickOrder.php';
@@ -84,6 +86,7 @@ class App
         require_once __DIR__ . '/../Controllers/CartColorController.php';
         require_once __DIR__ . '/../Controllers/FavoriteController.php';
         require_once __DIR__ . '/../Controllers/AuthController.php';
+        require_once __DIR__ . '/../Controllers/CustomerAccountController.php';
         require_once __DIR__ . '/../Controllers/InvitationController.php';
         require_once __DIR__ . '/../Controllers/OrderController.php';
         require_once __DIR__ . '/../Controllers/QuickOrderController.php';
