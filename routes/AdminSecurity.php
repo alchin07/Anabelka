d110e9e2a4682da42760b7726257d5210daeef4a
alchin07 <almobis@gmail.com>
2026-09-08 19:07:12 +0200

    Add administrator security routes

---
+++ routes/AdminSecurity.php
@@ -0,0 +1,41 @@
+<?php
+
+$router->get(
+    '/admin/administrators',
+    'AdminAdministratorController@index'
+);
+
+$router->post(
+    '/admin/administrators/create',
+    'AdminAdministratorController@create'
+);
+
+$router->post(
+    '/admin/administrators/role',
+    'AdminAdministratorController@changeRole'
+);
+
+$router->post(
+    '/admin/administrators/toggle',
+    'AdminAdministratorController@toggle'
+);
+
+$router->post(
+    '/admin/administrators/password',
+    'AdminAdministratorController@resetPassword'
+);
+
+$router->post(
+    '/admin/administrators/roles/create',
+    'AdminAdministratorController@createRole'
+);
+
+$router->post(
+    '/admin/administrators/roles/permissions',
+    'AdminAdministratorController@updateRolePermissions'
+);
+
+$router->get(
+    '/admin/audit',
+    'AdminAdministratorController@audit'
+);
