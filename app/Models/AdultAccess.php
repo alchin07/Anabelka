88009a3a7f2ae5fd6ab203d30669b9a4d9665f05
alchin07 <almobis@gmail.com>
2026-09-09 10:45:11 +0200

    Connect adult access to customer age preferences

--- app/Models/AdultAccess.php
+++ app/Models/AdultAccess.php
@@ -7,13 +7,62 @@ class AdultAccess
 
     public static function isConfirmed()
     {
-        return !empty($_SESSION[self::SESSION_KEY]);
+        return self::canShowAdultContent()
+            || !empty($_SESSION[self::SESSION_KEY]);
+    }
+
+
+    public static function canShowAdultContent()
+    {
+        if (empty($_SESSION['user_id'])) {
+            return false;
+        }
+
+        try {
+            $user = CustomerAccount::current();
+
+            return is_array($user)
+                && !empty($user['is_adult'])
+                && !empty($user['show_adult']);
+        } catch (Throwable $e) {
+            return false;
+        }
+    }
+
+
+    public static function isKnownUnderage()
+    {
+        if (empty($_SESSION['user_id'])) {
+            return false;
+        }
+
+        try {
+            $profile = CustomerProfile::getForUser((int) $_SESSION['user_id']);
+            $birthDate = trim((string) ($profile['birth_date'] ?? ''));
+
+            return $birthDate !== ''
+                && !CustomerProfile::isAdultBirthDate($birthDate);
+        } catch (Throwable $e) {
+            return false;
+        }
     }
 
 
     public static function confirm()
     {
+        if (self::isKnownUnderage()) {
+            throw new RuntimeException(
+                'Розділ 18+ недоступний неповнолітнім користувачам.'
+            );
+        }
+
         $_SESSION[self::SESSION_KEY] = time();
+    }
+
+
+    public static function clearConfirmation()
+    {
+        unset($_SESSION[self::SESSION_KEY]);
     }
 
 
