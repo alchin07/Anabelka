a51b1809181c9ea0eabf76eeffb96d4b44c13269
alchin07 <almobis@gmail.com>
2026-09-09 16:28:58 +0200

    Load admin notification center module

--- app/Core/App.php
+++ app/Core/App.php
@@ -53,6 +53,7 @@ class App
         require_once __DIR__ . '/../Models/UserRankTranslator.php';
         require_once __DIR__ . '/../Models/UserInvitation.php';
         require_once __DIR__ . '/../Models/AdminAccess.php';
+        require_once __DIR__ . '/../Models/AdminNotificationCenter.php';
         require_once __DIR__ . '/../Models/AdminActionAudit.php';
         require_once __DIR__ . '/../Models/AdminManagement.php';
         require_once __DIR__ . '/../Models/AdminRolePermission.php';
