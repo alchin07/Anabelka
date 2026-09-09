8b023546d06c6b9ada34c3f28721671f64f4a482
alchin07 <almobis@gmail.com>
2026-09-09 12:51:25 +0200

    Load customer rank request models

--- app/Core/App.php
+++ app/Core/App.php
@@ -33,6 +33,8 @@ class App
         require_once __DIR__ . '/../Models/CustomerAddress.php';
         require_once __DIR__ . '/../Models/CustomerAccount.php';
         require_once __DIR__ . '/../Models/CustomerAccountInterfaceTranslator.php';
+        require_once __DIR__ . '/../Models/CustomerRankRequest.php';
+        require_once __DIR__ . '/../Models/CustomerRankRequestInterfaceTranslator.php';
         require_once __DIR__ . '/../Models/AdminUser.php';
         require_once __DIR__ . '/../Models/Order.php';
         require_once __DIR__ . '/../Models/QuickOrder.php';
