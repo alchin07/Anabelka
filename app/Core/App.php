7dfe4ab79b6187a06611898821a16e1bfa22289b
alchin07 <almobis@gmail.com>
2026-09-10 21:27:38 +0200

    Wire password reset module into app

--- app/Core/App.php
+++ app/Core/App.php
@@ -32,12 +32,14 @@ class App
         require_once __DIR__ . '/../Models/CustomerProfile.php';
         require_once __DIR__ . '/../Models/CustomerAddress.php';
         require_once __DIR__ . '/../Models/CustomerEmailVerification.php';
+        require_once __DIR__ . '/../Models/CustomerPasswordReset.php';
         require_once __DIR__ . '/../Models/RegistrationConsent.php';
         require_once __DIR__ . '/../Models/PasswordPolicyInterfaceTranslator.php';
         require_once __DIR__ . '/../Models/PasswordPolicy.php';
         require_once __DIR__ . '/../Models/CustomerAccount.php';
         require_once __DIR__ . '/../Models/CustomerAccountInterfaceTranslator.php';
         require_once __DIR__ . '/../Models/CustomerEmailVerificationInterfaceTranslator.php';
+        require_once __DIR__ . '/../Models/PasswordResetInterfaceTranslator.php';
         require_once __DIR__ . '/../Models/RegistrationInterfaceTranslator.php';
         require_once __DIR__ . '/../Models/CustomerRankRequest.php';
         require_once __DIR__ . '/../Models/CustomerRankRequestInterfaceTranslator.php';
@@ -88,6 +90,7 @@ class App
         require_once __DIR__ . '/../Services/TranslationDashboardService.php';
         require_once __DIR__ . '/../Services/EmailVerificationMailer.php';
         require_once __DIR__ . '/../Services/EmailVerificationService.php';
+        require_once __DIR__ . '/../Services/PasswordResetService.php';
 
         require_once __DIR__ . '/../Controllers/HomeController.php';
         require_once __DIR__ . '/../Controllers/CatalogController.php';
@@ -100,6 +103,7 @@ class App
         require_once __DIR__ . '/../Controllers/AuthController.php';
         require_once __DIR__ . '/../Controllers/CustomerAccountController.php';
         require_once __DIR__ . '/../Controllers/EmailVerificationController.php';
+        require_once __DIR__ . '/../Controllers/PasswordResetController.php';
         require_once __DIR__ . '/../Controllers/LegalController.php';
         require_once __DIR__ . '/../Controllers/InvitationController.php';
         require_once __DIR__ . '/../Controllers/OrderController.php';
@@ -130,6 +134,7 @@ class App
         require __DIR__ . '/../../routes/AdminSecurity.php';
         require __DIR__ . '/../../routes/CustomerAccount.php';
         require __DIR__ . '/../../routes/Legal.php';
+        require __DIR__ . '/../../routes/PasswordReset.php';
         require __DIR__ . '/../../routes/Web.php';
         require __DIR__ . '/../../routes/Adult.php';
         require __DIR__ . '/../../routes/CartColor.php';
