6a8c64b0e77a1e5ec2f33afe0473a960449401c9
alchin07 <almobis@gmail.com>
2026-09-10 21:49:27 +0200

    Add social auth configuration

---
+++ config/social-auth.php
@@ -0,0 +1,37 @@
+<?php
+
+$config = [
+    'providers' => [
+        'google' => [
+            'client_id' => getenv('GOOGLE_CLIENT_ID') ?: '',
+            'client_secret' => getenv('GOOGLE_CLIENT_SECRET') ?: '',
+            'redirect_uri' => getenv('GOOGLE_REDIRECT_URI') ?: '',
+            'authorization_endpoint' => 'https://accounts.google.com/o/oauth2/v2/auth',
+            'token_endpoint' => 'https://oauth2.googleapis.com/token',
+            'userinfo_endpoint' => 'https://openidconnect.googleapis.com/v1/userinfo',
+            'scope' => 'openid email profile',
+            'timeout' => 20
+        ]
+    ]
+];
+
+$localConfigFile = __DIR__ . '/social-auth.local.php';
+
+if (is_file($localConfigFile)) {
+    $localConfig = require $localConfigFile;
+
+    if (is_array($localConfig)) {
+        foreach ($localConfig['providers'] ?? [] as $provider => $providerConfig) {
+            if (!is_array($providerConfig)) {
+                continue;
+            }
+
+            $config['providers'][$provider] = array_merge(
+                $config['providers'][$provider] ?? [],
+                $providerConfig
+            );
+        }
+    }
+}
+
+return $config;
