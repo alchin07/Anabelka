69aa1970c39b374454a983c3cc78971ec1cec724
alchin07 <almobis@gmail.com>
2026-09-12 08:21:02 +0200

    Wire system error workflow statuses

--- routes/AdminSecurity.php
+++ routes/AdminSecurity.php
@@ -2,6 +2,7 @@
 
 require_once __DIR__ . '/../app/Models/SystemErrorLog.php';
 require_once __DIR__ . '/../app/Models/SystemErrorNotification.php';
+require_once __DIR__ . '/../app/Models/SystemErrorStatus.php';
 require_once __DIR__ . '/../app/Controllers/AdminSystemErrorController.php';
 require_once __DIR__ . '/../app/Controllers/AdminSystemErrorNotificationController.php';
 require_once __DIR__ . '/../app/Controllers/AdminErrorTestController.php';
@@ -69,6 +70,11 @@ $router->get(
 $router->get(
     '/admin/system/errors',
     'AdminSystemErrorController@index'
+);
+
+$router->post(
+    '/admin/system/errors/status',
+    'AdminSystemErrorController@updateStatus'
 );
 
 $router->get(
