9636e869b4b946ad225df9993776aa7a241dd333
alchin07 <almobis@gmail.com>
2026-09-15 07:07:22 +0200

    Load reusable Anabelka select in admin

--- views/admin/partials/header.php
+++ views/admin/partials/header.php
@@ -157,6 +157,10 @@ $canAudit = $adminCan('audit.view');
     rel="stylesheet"
     href="/Anabelka/css/anabelka-dialog.css?v=1"
 >
+<link
+    rel="stylesheet"
+    href="/Anabelka/css/anabelka-select.css?v=1"
+>
 
 <header class="admin-site-header">
     <div class="admin-topbar">
@@ -430,6 +434,7 @@ $canAudit = $adminCan('audit.view');
     </a>
 </aside>
 
+<script src="/Anabelka/js/anabelka-select.js?v=1"></script>
 <script src="/Anabelka/js/anabelka-dialog.js?v=1"></script>
 <script src="/Anabelka/js/admin-ui-focus-policy.js?v=2"></script>
 <script src="/Anabelka/js/admin-product-preview.js?v=1"></script>
