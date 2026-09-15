3febcc88125493e47ba15a0ffd6f8f3bd3b505dc
alchin07 <almobis@gmail.com>
2026-09-15 06:15:58 +0200

    Load shared branded admin dialog

--- views/admin/partials/header.php
+++ views/admin/partials/header.php
@@ -153,6 +153,10 @@ $canAudit = $adminCan('audit.view');
     rel="stylesheet"
     href="/Anabelka/css/admin-product-editor-fixes.css?v=1"
 >
+<link
+    rel="stylesheet"
+    href="/Anabelka/css/anabelka-dialog.css?v=1"
+>
 
 <header class="admin-site-header">
     <div class="admin-topbar">
@@ -426,9 +430,10 @@ $canAudit = $adminCan('audit.view');
     </a>
 </aside>
 
+<script src="/Anabelka/js/anabelka-dialog.js?v=1"></script>
 <script src="/Anabelka/js/admin-ui-focus-policy.js?v=2"></script>
 <script src="/Anabelka/js/admin-product-preview.js?v=1"></script>
 <script defer src="/Anabelka/js/admin-product-variant-stock.js?v=2"></script>
 <script defer src="/Anabelka/js/admin-product-editor-fixes.js?v=1"></script>
 <script defer src="/Anabelka/js/admin-order-variants.js?v=1"></script>
-<script src="/Anabelka/js/admin-nav.js?v=17"></script>
+<script src="/Anabelka/js/admin-nav.js?v=18"></script>
