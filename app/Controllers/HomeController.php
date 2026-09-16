ec18b598ffa72c973bf9944ba74cfd08c1a88370
alchin07 <almobis@gmail.com>
2026-09-16 17:26:13 +0200

    feat: load home news and reviews safely

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -25,13 +25,35 @@ class HomeController extends Controller
             $languageCode
         );
 
+        $homeNews = [];
+        $homeReviews = [];
+
+        try {
+            $homeNews = SiteNews::latestPublished(
+                3,
+                $languageCode
+            );
+        } catch (Throwable $e) {
+            error_log('Home news: ' . $e->getMessage());
+            $homeNews = [];
+        }
+
+        try {
+            $homeReviews = ProductReview::latestApprovedStandard(2);
+        } catch (Throwable $e) {
+            error_log('Home reviews: ' . $e->getMessage());
+            $homeReviews = [];
+        }
+
         $this->view(
             'home',
             [
                 'currentLanguage' => $currentLanguage,
                 'directions' => $directions,
                 'navigationTree' => $navigationTree,
-                'latestProducts' => $latestProducts
+                'latestProducts' => $latestProducts,
+                'homeNews' => $homeNews,
+                'homeReviews' => $homeReviews
             ]
         );
     }
