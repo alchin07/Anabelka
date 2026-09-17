658798e8078a4e0349fca94f34e0c34c6076ac58
alchin07 <almobis@gmail.com>
2026-09-17 21:33:02 +0200

    feat: register storefront information routes

--- routes/StorefrontPages.php
+++ routes/StorefrontPages.php
@@ -2,3 +2,5 @@
 
 $router->get('/discounts', 'StorefrontPageController@discounts');
 $router->get('/new', 'StorefrontPageController@newArrivals');
+$router->get('/delivery-payment', 'StorefrontPageController@deliveryPayment');
+$router->get('/contacts', 'StorefrontPageController@contacts');
