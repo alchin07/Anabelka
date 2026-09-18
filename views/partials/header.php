a87df47161a599870921998c45c88fe9dccf5e9e
alchin07 <almobis@gmail.com>
2026-09-18 23:04:16 +0200

    Cache-bust catalog scrollbar styling

--- views/partials/header.php
+++ views/partials/header.php
@@ -218,7 +218,7 @@ if (
 
     <link
         rel="stylesheet"
-        href="/Anabelka/css/public-catalog-sidebar.css?v=2"
+        href="/Anabelka/css/public-catalog-sidebar.css?v=3"
     >
 
     <link
