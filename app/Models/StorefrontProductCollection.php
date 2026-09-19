3873059a9d80f0712f10358bde03f56fc7c3b671
alchin07 <almobis@gmail.com>
2026-09-19 17:57:41 +0200

    Expose independent product colors in storefront collections

--- app/Models/StorefrontProductCollection.php
+++ app/Models/StorefrontProductCollection.php
@@ -131,7 +131,7 @@ class StorefrontProductCollection
             },
             $products
         );
-        $colorVariants = ProductImage::colorVariantsForProducts(
+        $colorVariants = ProductColor::variantsForProducts(
             $productIds
         );
 
