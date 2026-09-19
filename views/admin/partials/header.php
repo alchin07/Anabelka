1aedacc2ef028b27dd8106e3d2f88df1407ab536
alchin07 <almobis@gmail.com>
2026-09-19 12:25:35 +0200

    Bump category select assets for thumbnail upload UI

--- views/admin/partials/header.php
+++ views/admin/partials/header.php
@@ -164,7 +164,7 @@ $canAudit = $adminCan('audit.view');
 >
 <link
     rel="stylesheet"
-    href="/Anabelka/css/anabelka-select.css?v=3"
+    href="/Anabelka/css/anabelka-select.css?v=4"
 >
 
 <header class="admin-site-header">
@@ -470,7 +470,7 @@ $canAudit = $adminCan('audit.view');
     </a>
 </aside>
 
-<script src="/Anabelka/js/anabelka-select.js?v=5"></script>
+<script src="/Anabelka/js/anabelka-select.js?v=6"></script>
 <script src="/Anabelka/js/anabelka-dialog.js?v=1"></script>
 <script src="/Anabelka/js/admin-ui-focus-policy.js?v=2"></script>
 <script src="/Anabelka/js/admin-product-preview.js?v=1"></script>
