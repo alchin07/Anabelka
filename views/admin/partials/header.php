4d2aa22f1c0a632e98f070580243e50ba69cd950
alchin07 <almobis@gmail.com>
2026-09-20 12:47:24 +0200

    Load admin Back guard before UI layers

--- views/admin/partials/header.php
+++ views/admin/partials/header.php
@@ -470,11 +470,12 @@ $canAudit = $adminCan('audit.view');
     </a>
 </aside>
 
-<script src="/Anabelka/js/anabelka-select.js?v=7"></script>
-<script src="/Anabelka/js/anabelka-dialog.js?v=1"></script>
+<script src="/Anabelka/js/anabelka-admin-back.js?v=1"></script>
+<script src="/Anabelka/js/anabelka-select.js?v=8"></script>
+<script src="/Anabelka/js/anabelka-dialog.js?v=2"></script>
 <script src="/Anabelka/js/admin-ui-focus-policy.js?v=2"></script>
 <script src="/Anabelka/js/admin-product-preview.js?v=1"></script>
 <script defer src="/Anabelka/js/admin-product-variant-stock.js?v=16"></script>
 <script defer src="/Anabelka/js/admin-product-editor-fixes.js?v=3"></script>
 <script defer src="/Anabelka/js/admin-order-variants.js?v=1"></script>
-<script src="/Anabelka/js/admin-nav.js?v=25"></script>
+<script src="/Anabelka/js/admin-nav.js?v=26"></script>
