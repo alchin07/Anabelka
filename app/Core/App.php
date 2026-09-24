c2a3f5e96e952d88441c065b715b53b0320828af
alchin07 <almobis@gmail.com>
2026-09-24 17:04:35 +0200

    Fix administrator invitation bootstrap and routes

--- app/Core/App.php
+++ app/Core/App.php
@@ -76,6 +76,7 @@ class App
         require_once __DIR__ . '/../Models/UserRankTranslator.php';
         require_once __DIR__ . '/../Models/UserInvitation.php';
         require_once __DIR__ . '/../Models/AdminAccess.php';
+        require_once __DIR__ . '/../Models/AdminInvitation.php';
         require_once __DIR__ . '/../Models/AdminNotificationCenter.php';
         require_once __DIR__ . '/../Models/AdminDashboardServiceRegistry.php';
         require_once __DIR__ . '/../Models/AdminActionAudit.php';
