f5a9a5da92f4f609b25207972e2c396a41ce650e
alchin07 <almobis@gmail.com>
2026-09-25 21:44:24 +0200

    Refresh personal contract styles

--- views/admin/administrators/profile.php
+++ views/admin/administrators/profile.php
@@ -80,7 +80,7 @@ $payoutLabels = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($pageTitle ?? 'Адмін-панель · Профіль') ?></title>
-    <link rel="stylesheet" href="/Anabelka/css/admin-profile.css?v=3">
+    <link rel="stylesheet" href="/Anabelka/css/admin-profile.css?v=4">
 </head>
 <body>
 
